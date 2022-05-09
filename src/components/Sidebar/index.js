d4088dfe3286011dbec696bc9f80e1440b57781c
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-05-09 14:41:52 -0400

    sidebar page icon

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,8 @@
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
 import LogoXM from '../../assets/images/logo-XM.jpeg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
   return (
@@ -10,7 +12,13 @@ const Sidebar = () => {
       </Link>
       <nav>
         <NavLink exact="true" activeclassname="active" to="/">
-
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
       </nav>
     </div>
