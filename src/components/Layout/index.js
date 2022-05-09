7c2c83d0c0d49ed3e12215cc1869f7a3770b324e
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-05-09 16:55:15 -0400

    home page setup

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,23 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar'
 import './index.scss'
 
 const Layout = () => {
   return (
-    <>
+    <div className='App'>
       <Sidebar />
-    </>
+      <div className='page'>
+        <span className='tags top-tags'>top tag</span>
+
+        <Outlet />
+
+        <span className='tags bottom-tags'>
+          bottom tag
+          <br />
+          <span className='bottom-tag-html'>html</span>
+        </span>
+      </div>
+    </div>
   )
 }
 
