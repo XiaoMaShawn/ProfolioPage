1331124849ca9c14fa19587464f6191742ce8b6e
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-05-10 15:06:10 -0400

    homepage animated letters

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,31 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = [' ', 'X', 'i', 'a', 'o', ' ', 'M', 'a'];
+  const jobArray = ['W', 'e', 'b', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
   return (
     <div className='container home-page'>
       <div className='text-zone'>
         <h1>
-          Hi, <br /> I'm Xiao Ma <br /> web developer
+          <span className='letterClass'>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+          <br />
+          <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={23} />
         </h1>
         <h2>
           Frontend Developer / JavaScript
