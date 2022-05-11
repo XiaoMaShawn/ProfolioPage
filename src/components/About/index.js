9881fef60af711dd9b8d147fb70b96efce0b1f9a
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-05-11 16:43:47 -0400

    spin cube in about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,8 @@
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters'
 import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -24,6 +26,26 @@ const About = () => {
         </h1>
         <p>I am good at nothing.</p>
         <p>I have no idea what to do.</p>
+      </div>
+
+      <div className='stage-cube-cont'>
+        <div className='cubespinner'>
+          <div className='face1'>
+            <FontAwesomeIcon icon={faReact} color='#5ed4f4' />
+          </div>
+          <div className='face2'>
+            <FontAwesomeIcon icon={faHtml5} color='#f06529' />
+          </div>
+          <div className='face3'>
+            <FontAwesomeIcon icon={faCss3} color='#28a4d9' />
+          </div>
+          <div className='face4'>
+            <FontAwesomeIcon icon={faJsSquare} color='#efd81d' />
+          </div>
+          <div className='face5'>
+            <FontAwesomeIcon icon={faGitAlt} color='#ec4d28' />
+          </div>
+        </div>
       </div>
     </div>
   )
