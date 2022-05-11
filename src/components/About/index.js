7980ce29ea2c9e5846c39646a89ee0bf7e2a8538
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-05-11 12:15:00 -0400

    about page text animation

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,30 @@
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters'
+import { useState, useEffect } from 'react';
 
 const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000);
+  }, []);
+
   return (
-    <div>
-      About page
+    <div className='container about-page'>
+      <div className='text-zone'>
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
+            idx={15}
+          />
+        </h1>
+        <p>I am good at nothing.</p>
+        <p>I have no idea what to do.</p>
+      </div>
     </div>
   )
 };
